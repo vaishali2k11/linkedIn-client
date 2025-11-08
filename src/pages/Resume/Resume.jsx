1d073ed8a6b110726c77f0f5ff6a9b7a5c120f1d
Rahul Rathor <rathore.vaishali1011@gmail.com>
2025-11-09 01:12:32 +0530

    update

--- src/pages/Resume/Resume.jsx
+++ src/pages/Resume/Resume.jsx
@@ -9,13 +9,16 @@ export const Resume = () => {
   }, []);
 
   return (
-    <div className="px-5 xl:px-50 py-9 flex gap-5 w-full mt-5 bg-gray-100">
-      <div className="w-full py-5 sm:w-[74%]">
-        <img
-          src={userData?.resume}
-          alt="resume"
-          className="w-full h-full rounded-lg"
-        />
+    <div className="px-5 xl:px-50 mt-[54px] flex gap-5 w-full bg-gray-100 min-h-[calc(100%-126px)] max-h-[calc(100%-126px)] h-full overflow-hidden">
+
+      <div className="my-3 max-h-[95%] min-h-[95%] h-full overflow-y-auto w-full sm:w-[74%]">
+        <div className="w-full">
+          <img
+            src={userData?.resume}
+            alt="resume"
+            className="w-full h-full rounded-lg"
+          />
+        </div>
       </div>
 
       <div className="w-[26%] py-5 hidden md:block">
